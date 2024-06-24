b87228c7c78d53aa0e547d3fc72b5e330bbda93a
aanshuvi <shahaanshuvi@gmail.com>
2024-06-24 11:06:28 +0530

    randomizing-food-locations

--- snake-game/food.py
+++ snake-game/food.py
@@ -1,4 +1,5 @@
 from turtle import Turtle
+import random
 
 class Food(Turtle):
     def __init__(self):
@@ -6,3 +7,11 @@ class Food(Turtle):
          self.shape("circle")  
          self.penup()
          self.shapesize(stretch_len=0.5,stretch_wid=0.5)
+         self.color("red")
+         self.speed("fastest")
+         self.refresh()
+
+    def refresh(self):
+        random_x = random.randint(-280,280)
+        random_y = random.randint(-280,280)
+        self.goto(random_x,random_y)
--- snake-game/main.py
+++ snake-game/main.py
@@ -1,5 +1,6 @@
 from turtle import Screen
 from snake import Snake
+from food import Food
 import time
 
 screen = Screen()
@@ -9,6 +10,7 @@ screen.title("The Ultimate Snake Game")
 screen.tracer(0)
 
 snake = Snake()
+food = Food()
 
 screen.listen()
 screen.onkey(snake.up, "Up")
@@ -22,4 +24,7 @@ while game_is_on:
     time.sleep(0.1)
     snake.move()
 
-screen.exitonclick()
+    if snake.head.distance(food) < 15:
+        food.refresh()
+
+screen.exitonclick() 
