4669613cc8c229040234ce26d611680736ab9b6d
aanshuvi <shahaanshuvi@gmail.com>
2024-06-25 16:00:30 +0530

    final-changes

--- states-game/main.py
+++ states-game/main.py
@@ -8,22 +8,23 @@ image = "states-game/blank_states_img.gif"
 screen.addshape(image)
 turtle.shape(image)
 
-game_is_on = True
 score = 0
 guessed_correctly = []
 
 df = pd.read_csv("states-game/50_states.csv")
 all_states = df["state"].to_list()
 
-while game_is_on:
-    answer_state = screen.textinput(title="Guess the state",prompt="What's the state's name?").title()
-    for state in all_states:
-        if answer_state == state:
-            score +=1
-            guessed_correctly.append(state)
-            x_cor = df['x']
-            y_cor = df['y']
-            turtle.goto(x_cor,y_cor)
-            turtle.write(state,font=("Arial",12,"normal"))
+while len(guessed_correctly)< len(all_states):
+    answer_state = screen.textinput(title=f"{score}/50 States correct",prompt="What's the state's name?").title()
+
+    if answer_state in all_states:
+        t = Turtle()
+        t.hideturtle()
+        t.penup()
+        score +=1
+        guessed_correctly.append(answer_state)
+        state_data = df[df["state"] == answer_state]
+        t.goto(int(state_data['x']),int(state_data['y']))
+        t.write(answer_state,font=("Arial",12,"normal"))
 
 screen.mainloop()
