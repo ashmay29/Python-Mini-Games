30485377911d901ba9b58db842b03b856fa8dce8
aanshuvi <shahaanshuvi@gmail.com>
2024-06-25 12:04:01 +0530

    adding-high-score-feature

--- snake-game/snake.py
+++ snake-game/snake.py
@@ -24,6 +24,13 @@ class Snake():
         for position in STARTING_POSITIONS:
             self.add_segment(position)
 
+    def reset(self):
+        for seg in self.segments:
+            seg.goto(1000,1000)
+        self.segments.clear()
+        self.create_snake()
+        self.head = self.segments[0]
+
     def extend(self):
         self.add_segment(self.segments[-1].position()) 
 
